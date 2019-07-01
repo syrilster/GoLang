dc01f78267a9c31041a9414efe2b0dc4473fb02e
syrilster <syrilster@gmail.com>
2019-07-01 11:35:54 +1000

    Go defer example updated

--- defers.go
+++ defers.go
@@ -5,7 +5,15 @@ import "fmt"
 //A defer statement defers the execution of a function until the surrounding function returns.
 // Like Java finally
 func main() {
-	defer fmt.Println("World")
 
 	fmt.Println("Hello")
+
+	//Deferred function calls are pushed onto a stack. When a function returns,
+	// its deferred calls are executed in last-in-first-out order.
+
+	for index := 0; index < 10; index++ {
+		defer fmt.Println(index)
+	}
+
+	fmt.Println("World")
 }
