1051688938e944ba108b252306a767bf7c409d8b
syrilster <syrilster@gmail.com>
2019-07-03 12:05:09 +1000

    Go channels

---
+++ channels.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func sum(input []int, channel chan int) {
+	sum := 0
+	for _, value := range input {
+		sum += value
+	}
+
+	channel <- sum
+}
+
+/*
+ch <- v    // Send v to channel ch.
+v := <-ch  // Receive from ch, and
+           // assign value to v.
+*/
+func main() {
+	randArray := []int{10, 20, 30, 4, 5, 6}
+	channel := make(chan int)
+
+	go sum(randArray[:len(randArray)/2], channel)
+	go sum(randArray[len(randArray)/2:], channel)
+
+	receiverOne, receiverTwo := <-channel, <-channel // receive from channel
+	fmt.Println(receiverOne, receiverTwo, receiverOne+receiverTwo)
+
+}
