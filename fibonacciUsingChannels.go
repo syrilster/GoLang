e0deb648982f3cd3189d42e33727e96d2e6a5291
syrilster <syrilster@gmail.com>
2019-07-03 13:31:07 +1000

    Updated the notes.

--- fibonacciUsingChannels.go
+++ fibonacciUsingChannels.go
@@ -12,6 +12,11 @@ func fibonacci(maxRange int, channel chan int) {
 	close(channel)
 }
 
+/*
+Note: Only the sender should close a channel, never the receiver. Sending on a closed channel will cause a panic.
+Another note: Channels aren't like files; you don't usually need to close them. Closing is only necessary when the
+receiver must be told there are no more values coming, such as to terminate a range loop.
+*/
 func main() {
 	channel := make(chan int)
 	go fibonacci(10, channel)
